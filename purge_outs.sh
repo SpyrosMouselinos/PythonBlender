0224d393d09b73a781fc148224152acc59a2c290
Spyros Entropy <mouselinos.spur.kw@gmail.com>
2021-04-27 18:50:49 +0200

    added this

---
+++ purge_outs.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#
+echo "Clearing Images..."
+rm -rf images
+mkdir images
+
+echo "Clearing scenes..."
+rm -rf scenes
+mkdir scenes
+
+echo "Clearing questions..."
+rm -rf questions
+mkdir questions
+
